2e5028e91b381f8e0002f24c5237d067a9958667
Zhero-Six <vmochoge@gmail.com>
2025-04-02 11:06:38 +0300

    updates helper.js

--- test/helpers.js
+++ test/helpers.js
@@ -1,24 +1,73 @@
-const chai = require('chai')
-global.expect = chai.expect
-global.chai = chai
-const fs = require('file-system')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
-const spies = require('chai-spies')
+// helpers.js
 
-chai.use( spies );
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+  return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: []
+  };
+}
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+function createEmployeeRecords(employeeData) {
+  return employeeData.map(createEmployeeRecord);
+}
 
-const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
-  }
-);
+function createTimeInEvent(dateTimeString) {
+  const [date, hour] = dateTimeString.split(' ');
+  this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour),
+      date
+  });
+  return this;
+}
 
-const src = babelResult.code
+function createTimeOutEvent(dateTimeString) {
+  const [date, hour] = dateTimeString.split(' ');
+  this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour),
+      date
+  });
+  return this;
+}
 
-jsdom({
-  html, src
-});
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find(event => event.date === date);
+  const timeOut = this.timeOutEvents.find(event => event.date === date);
+  return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  const hours = this.hoursWorkedOnDate(date);
+  return hours * this.payPerHour;
+}
+
+function allWagesFor() {
+  const dates = this.timeInEvents.map(event => event.date);
+  return dates.reduce((total, date) => total + this.wagesEarnedOnDate(date), 0);
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce((total, employee) => total + employee.allWagesFor(), 0);
+}
+
+// Export all functions
+module.exports = {
+  createEmployeeRecord,
+  createEmployeeRecords,
+  createTimeInEvent,
+  createTimeOutEvent,
+  hoursWorkedOnDate,
+  wagesEarnedOnDate,
+  allWagesFor,
+  findEmployeeByFirstName,
+  calculatePayroll
+};
